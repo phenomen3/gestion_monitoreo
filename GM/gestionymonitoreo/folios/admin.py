4b7d8b1e6059d1ca280c4342392c1f7495e9aa44
phenomen3 <fabrayan.r@gmail.com>
2023-12-29 00:01:28 +0000

    ADD: funcion para acortar la descripcion

--- GM/gestionymonitoreo/folios/admin.py
+++ GM/gestionymonitoreo/folios/admin.py
@@ -6,19 +6,22 @@ class FolioAdmin(admin.ModelAdmin):
     model = Folio
     fields = ('folio_id', 'motivo', 'usuario', 'descripcion', 'fecha', 'hora', 'ubicacion')
     exclude = ['creador']
-    list_display = ('folio_id', 'motivo', 'usuario', 'descripcion', 'fecha', 'ubicacion', 'nombre_sector', 'creador',)
+    list_display = ('folio_id', 'motivo', 'usuario', 'descripcion_corta', 'fecha', 'ubicacion', 'nombre_sector', 'creador',)
     # ordering = ['fecha']
     def nombre_sector(self, obj):
         return obj.ubicacion.sector  # Accedemos al campo 'sector' del modelo relacionado
 
+    def descripcion_corta(self, obj): # metodo para acortar la descripcion a mostrar
+        max_length = 30  # Puedes ajustar este valor según tu preferencia
+        truncated_description = f"{obj.descripcion[:max_length]}..." if len(obj.descripcion) > max_length else obj.descripcion
+        return truncated_description
+
     nombre_sector.short_description = 'Sector'
+    descripcion_corta.short_description = 'Descripción Corta'
+    #descripcion.short_description = 'descripcion'
     list_filter = ('creador', 'fecha')  # Deja solo los campos apropiados para filtrar
     search_fields = ('folio_id','motivo__nombre', 'descripcion', 'usuario')
 
-    # def short_description(self, obj):
-    #     max_length = 50  # You can adjust this value
-    #     return f"{obj.folio.descripcion[:max_length]}..." if len(obj.folio.descripcion) > max_length else obj.folio.descripcion
-    # short_description.short_description = 'Descripcion'  # Set the column header in the admin
 
     list_per_page = 20
 
