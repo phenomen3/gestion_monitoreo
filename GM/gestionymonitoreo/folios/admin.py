9020b2366163a37728e240665dfcea8dd648ad53
phenomen3 <fabrayan.r@gmail.com>
2024-01-05 15:18:55 +0000

    ADD: clase: textolargowidget

--- GM/gestionymonitoreo/folios/admin.py
+++ GM/gestionymonitoreo/folios/admin.py
@@ -1,23 +1,32 @@
 from django.contrib import admin
 from .models import Ubicacion, Motivo, Submotivo, Folio
+from django.db import models 
+from .forms import TextoLargoWidget
+#from django import forms
 
+class TextoLargoWidgetAdmin(admin.ModelAdmin):
+    form = TextoLargoWidget
 
 class FolioAdmin(admin.ModelAdmin):
+    
+    
     model = Folio
+    formfield_overrides = {
+        models.TextField: {'widget': TextoLargoWidget},
+    }
+
+
     fields = ('folio_id', 'motivo', 'usuario', 'descripcion', 'fecha', 'hora', 'ubicacion')
     exclude = ['creador']
-    list_display = ('folio_id', 'motivo', 'usuario', 'descripcion_corta', 'fecha', 'ubicacion', 'nombre_sector', 'creador',)
+    list_display = ('folio_id', 'motivo', 'usuario', 'descripcion', 'fecha', 'ubicacion', 'nombre_sector', 'creador',)
     # ordering = ['fecha']
     def nombre_sector(self, obj):
         return obj.ubicacion.sector  # Accedemos al campo 'sector' del modelo relacionado
 
-    def descripcion_corta(self, obj): # metodo para acortar la descripcion a mostrar
-        max_length = 30  # Puedes ajustar este valor según tu preferencia
-        truncated_description = f"{obj.descripcion[:max_length]}..." if len(obj.descripcion) > max_length else obj.descripcion
-        return truncated_description
+    
 
     nombre_sector.short_description = 'Sector'
-    descripcion_corta.short_description = 'Descripción Corta'
+    #descripcion_corta.short_description = 'Descripción Corta'
     #descripcion.short_description = 'descripcion'
     list_filter = ('creador', 'fecha')  # Deja solo los campos apropiados para filtrar
     search_fields = ('folio_id','motivo__nombre', 'descripcion', 'usuario')
@@ -30,7 +39,6 @@ class FolioAdmin(admin.ModelAdmin):
             obj.creador = request.user
         super().save_model(request, obj, form, change)
 
-
 class SubmotivoAdmin(admin.ModelAdmin):
     model = Submotivo
     fields=('nombre', 'motivo','id')
@@ -38,11 +46,11 @@ class SubmotivoAdmin(admin.ModelAdmin):
     search_fields = ('motivo__nombre','nombre')
     # Submotivo.objects.filter(autor__nombre__icontains="Smith")
 
-
 admin.site.register(Folio, FolioAdmin)
 admin.site.register(Motivo)
 admin.site.register(Submotivo, SubmotivoAdmin)
 admin.site.register(Ubicacion)
+#admin.site.register(TextoLargoWidget, TextoLargoWidgetAdmin)
 # admin.site.register(Alcaldia)
 # admin.site.register(C2)
 # admin.site.register(Sector)
--- GM/gestionymonitoreo/folios/forms.py
+++ GM/gestionymonitoreo/folios/forms.py
@@ -1,7 +1,19 @@
 from django import forms
 from .models import Folio
 
+class TextoLargoWidget(forms.Textarea):
+    def _init_(self, attrs=None):
+        default_attrs = {'rows': 3, 'style': 'overflow: hidden; text-overflow: ellipsis; white-space: nowrap; max-height: 3.6em;'}
+        if attrs:
+            default_attrs.update(attrs)
+        super()._init_(default_attrs)
+        #return default_attrs
+
 class FolioForm(forms.ModelForm):
     class Meta:
         model = Folio
-        fields = ['folio_id', 'usuario', 'descripcion', 'colonia']
+        fields = ['folio_id', 'usuario', 'descripcion']
+        #fields = ['folio_id', 'usuario', 'descripcion', 'colonia'] se elimino 'colonia' por el error: File "/home/mic5/monitoreoygestion/gestion_monitoreo/mento/lib/python3.10/site-packages/django/forms/models.py", line 331, in __new__
+        # raise FieldError(message)
+        # django.core.exceptions.FieldError: Unknown field(s) ( colonia) specified for Folio
+
