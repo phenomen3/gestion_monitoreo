6283eec8f7524104146c2f0826735557081d135c
phenomen3 <fabrayan.r@gmail.com>
2024-01-08 16:36:58 +0000

    ADD: descripcion corta con max lines 3

--- GM/gestionymonitoreo/folios/admin.py
+++ GM/gestionymonitoreo/folios/admin.py
@@ -1,32 +1,43 @@
 from django.contrib import admin
 from .models import Ubicacion, Motivo, Submotivo, Folio
-from django.db import models 
-from .forms import TextoLargoWidget
+#from django.db import models 
+#from .forms import TextoLargoWidget
 #from django import forms
 
-class TextoLargoWidgetAdmin(admin.ModelAdmin):
-    form = TextoLargoWidget
+# class TextoLargoWidgetAdmin(admin.ModelAdmin):
+#     form = TextoLargoWidget
 
 class FolioAdmin(admin.ModelAdmin):
     
     
     model = Folio
-    formfield_overrides = {
-        models.TextField: {'widget': TextoLargoWidget},
-    }
+    # formfield_overrides = { 
+    #     models.TextField: {'widget': TextoLargoWidget},
+    # }
 
 
     fields = ('folio_id', 'motivo', 'usuario', 'descripcion', 'fecha', 'hora', 'ubicacion')
     exclude = ['creador']
-    list_display = ('folio_id', 'motivo', 'usuario', 'descripcion', 'fecha', 'ubicacion', 'nombre_sector', 'creador',)
+    list_display = ('folio_id', 'motivo', 'usuario', 'descripcion_corta', 'fecha', 'ubicacion', 'nombre_sector', 'creador',)
     # ordering = ['fecha']
     def nombre_sector(self, obj):
         return obj.ubicacion.sector  # Accedemos al campo 'sector' del modelo relacionado
 
-    
+    def descripcion_corta(self, obj): # metodo para acortar la descripcion a mostrar
+        print("Ingreso el metodo")
+        
+        max_lines = 3
+        lines = obj.descripcion.splitlines()[:max_lines]
+        truncated_description = '\n'.join(lines)
+
+        max_length = 60  # Puedes ajustar este valor según tu preferencia
+        if len(truncated_description) > max_length:
+            truncated_description = f"{truncated_description[:max_length]}..."
+        
+        return truncated_description
 
     nombre_sector.short_description = 'Sector'
-    #descripcion_corta.short_description = 'Descripción Corta'
+    descripcion_corta.short_description = 'Descripción Corta'
     #descripcion.short_description = 'descripcion'
     list_filter = ('creador', 'fecha')  # Deja solo los campos apropiados para filtrar
     search_fields = ('folio_id','motivo__nombre', 'descripcion', 'usuario')
